10d12b14b859732269f26a1fd97f8b2db288c588
Tianyang Li <tmy1018@gmail.com>
2012-03-31 16:45:00 +0800

    what to do about bias?

--- test_mle_1.py
+++ test_mle_1.py
@@ -21,11 +21,12 @@
 # Copyright (2012) Tianyang Li
 # tmy1018@gmail.com
 
+from __future__ import division
 import getopt
 import sys
 import random
 
-import mle_1
+from mle_1 import est
 
 def main(args):
     L, N, runs, d_max, bound = None, None, None, None, None
@@ -33,6 +34,7 @@ def main(args):
         opts, args = getopt.getopt(args, 'L:N:r:d:b:')
     except getopt.GetoptError as err:
         print >> sys.stderr, str(err)
+        sys.exit(1)
     for opt, arg in opts:
         if opt == '-L':
             L = int(arg)
@@ -55,6 +57,29 @@ def main(args):
         start_pos = L * [0]
         for each_read in xrange(N):
             start_pos[random.randint(0, L - 1)] += 1
+        prev_pos = None
+        c, n = 0, 0
+        for read_pos in xrange(len(start_pos)):
+            if start_pos[read_pos] > 0:
+                if prev_pos != None:
+                    if read_pos - prev_pos > d_max:
+                        if n >= 3:
+                            L_est, N_est, lh = est(c, n, d_max)
+                            if c / L_est >= bound and n / N_est >= bound:
+                                print L_est, N_est
+                            c, n = 0, start_pos[read_pos]
+                    else:
+                        c += read_pos - prev_pos
+                        n += start_pos[read_pos]
+                else:
+                    n = start_pos[read_pos]
+                prev_pos = read_pos
+        if n != 0:
+            if n >= 3:
+                L_est, N_est, lh = est(c, n, d_max)
+                if c / L_est >= bound and n / N_est >= bound:
+                    print L_est, N_est
         
-
+if __name__ == '__main__':
+    main(sys.argv[1:])
 
