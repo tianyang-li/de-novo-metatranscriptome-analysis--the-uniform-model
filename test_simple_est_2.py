e2247f5204540ad0c14c617ebf04b692ace2b5d1
Tianyang Li <tmy1018@gmail.com>
2012-04-06 10:15:56 +0800

    see how it goes??

---
+++ test_simple_est_2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# This file is part of de_novo_uniform_metatranscriptome.
+# 
+# de_novo_uniform_metatranscriptome is free software: 
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# de_novo_uniform_metatranscriptome is distributed in 
+# the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with de_novo_uniform_metatranscriptome.  
+# If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright (2012) Tianyang Li
+# tmy1018@gmail.com
+
+import getopt
+import sys
+import random
+
+from random_contig_gen import rand_cont
+
+def main(args):
+    L, N, d, r = None, None, None, None
+    try:
+        opts, args = getopt.getopt(args, 'L:N:d:r:')
+    except getopt.GetoptError as err:
+        print >> sys.stderr, str(err)
+        sys.exit(1)
+    for opt, arg in opts:
+        if opt == '-L':
+            L = int(arg)
+        if opt == '-N':
+            N = int(arg)
+        if opt == '-d':
+            d = int(arg)
+        if opt == '-r':
+            r = int(arg)
+    if L == None or N == None or d == None or r == None:
+        print >> sys.stderr, "missing options"
+        sys.exit(1)
+    
+    random.seed()
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+
