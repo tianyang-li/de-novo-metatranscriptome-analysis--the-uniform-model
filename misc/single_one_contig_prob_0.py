2b63271f7d26c0471eb77050f56bdf9f2f229f50
Tianyang Li <tmy1018@gmail.com>
2012-04-30 16:48:21 +0800

    nhtdryjhrrh

--- misc/single_one_contig_prob_0.py
+++ misc/single_one_contig_prob_0.py
@@ -6,7 +6,20 @@ import random
 
 def main(args):
     d_max, read_len = None, None
+    prob_max =None
     MAX_LEN = 6000
+    try:
+        opts, args = getopt.getopt(args, 'd:l:u:')
+    except getopt.Get as err:
+        print >> sys.stderr, str(err)
+        sys.exit(1)
+    for opt,arg in opts:
+        if opt=='-l':
+        if opt=='-d':
+        if opt=='-u':
+    if d_max ==None or read_len==None or prob_max==None:
+        print >> sys.stderr, "missing options"
+        sys.exit(1)
 
 if __name__ == '__init__':
     main(sys.argv[1:])
