7ac60970e7b7bce9b2ce8fd8882a9ca7d055bcf8
Tianyang Li <tmy1018@gmail.com>
2012-04-30 16:42:03 +0800

    get me something that i can see

---
+++ misc/single_one_contig_prob_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import getopt
+import sys
+import random
+
+def main(args):
+    d_max, read_len = None, None
+    MAX_LEN = 6000
+
+if __name__ == '__init__':
+    main(sys.argv[1:])
+
+
