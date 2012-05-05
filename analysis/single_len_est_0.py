4a339b062e583a6d1456f828ddd788aca66209e3
Tianyang Li <tmy1018@gmail.com>
2012-05-05 15:59:51 +0800

    do it ASAP

--- analysis/single_len_est_0.py
+++ analysis/single_len_est_0.py
@@ -20,26 +20,37 @@ calculate estimated length
 """
 
 from __future__ import division
+
 import getopt
 import sys
+
 from HTSeq import SAM_Reader
 from Bio import SeqIO
+
 from math import log
+
+from single_one_contig_0 import find_one_contig_N
 
 def single_est_len(l, n, read_len):
     
 
 class SingleOneContig(object):
-    def __init__(self, d_max):
-        SIM_RUNS = 24000
-        MAX_LEN = 6000
+    def __init__(self, d_max, read_len):
+        self.MAX_LEN = 6000
+        self.N = find_one_contig_N(0.01, 0.95, self.MAX_LEN, d_max, read_len)
+        self.d_max = d_max
+        self.read_len = read_len
     
     def __call__(self, l, n):
-        
+        if (n / (l - self.read_len + 2 * self.d_max + 1) 
+            > self.N / (self.MAX_LEN - self.read_len + 1)):
+            return True
+        return False
 
 def main(args):
     sam_file, assembled_file = None, None
     read_len, assembly_kmer = None, None
+    
     try:
         opts, args = getopt.getopt(args, 'a:s:l:k:')
     except getopt.GetoptError as err:
@@ -57,6 +68,7 @@ def main(args):
     if sam_file == None or assembled_file == None or assembly_kmer == None or read_len == None:
         print >> sys.stderr, "missing input"
         sys.exit(1)
+        
     contigs = {}
     for rec in SeqIO.parse(assembled_file, 'fasta'):
         # length, read_count
@@ -65,8 +77,10 @@ def main(args):
         if align.aligned:
             if align.iv.chrom in contigs:
                 contigs[align.iv.chrom][1] += 1
+                
     d_max = read_len - assembly_kmer + 1  # max difference between 2 read starting positions
     keep_contig = SingleOneContig(d_max)
+    
     for contig, contig_stat in contigs.iteritems():
         if contig_stat[1] != 0:
             if keep_contig(contig_stat[0], contig_stat[1]):
