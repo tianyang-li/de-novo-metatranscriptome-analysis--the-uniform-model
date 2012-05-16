a61583a52f39be723349a8f8706a5e881d620995
Tianyang Li <tmy1018@gmail.com>
2012-05-16 09:08:22 +0800

    gfeoirgjeirpj

---
+++ analysis/psl_reader_0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2012 Tianyang Li
+#  tmy1018@gmail.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+class PslEntry(object):
+    def __init__(self, row):
+        """
+        row is a list of strings of each psl line
+        """
+
