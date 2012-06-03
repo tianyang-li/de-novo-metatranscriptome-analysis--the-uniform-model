af1833e73139806b7b4c7ecbd2a58a9635690335
Tianyang Li <tmy1018@gmail.com>
2012-06-03 14:33:30 +0800

    do it faster

---
+++ align_stats/max_median_0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+def main(args):
+    with open(args[0]) as fin:
+        for line in fin:
+            line = line.strip().split(" ")
+            if line[-1] == "1" and int(line[5]) <= 6000 and int(line[6]) < 10:
+                print line[2], # est
+                print line[5], # true
+                print int(line[6]) * int (line[7]),
+                print line[6], # median
+                print line[7], # max
+                print
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
---
+++ analysis/paired_len_est_0.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2012 Tianyang Li
+#  tmy1018@gmail.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+import getopt
+import sys
+
+from math import log
+
+def paired_len_est(contig, frags):
+    """
+    contig    contig length
+    frags     list of fragments' lengths
+    """
+
+def main(args):
+    contig = None
+    frags = []
+    if not contig or not frags:
+        print >> sys.stderr, "missing"
+        return 0
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])    
+
